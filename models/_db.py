fcf34846f98cb70c3208c673bd99f8fc569a67cb
Angel Shau <shauangel2000@gmail.com>
2023-09-13 09:43:35 +0800

    basic tools

---
+++ models/_db.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 21:46:58 2021
+
+@author: linxiangling
+"""
+
+from pymongo import MongoClient
+import pytz
+
+# DB = MongoClient('mongodb+srv://pqamanager:pass123word@pqacluster0.umi6y.mongodb.net/test')['PQAbot']
+DB = MongoClient('mongodb+srv://psabot:psabot2022@psabot.dt5vbbu.mongodb.net/test')['PSAbot']
+
+TAG_COLLECTION = DB['Tag']
+USER_COLLECTION = DB['User']
+INNER_POST_COLLECTION = DB['InnerPost']
+OUTER_DATA_COLLECTION = DB['OuterData']
+OUTER_DATA_CACHE_COLLECTION = DB['OuterDataCache']
+FAQ_DATA_COLLECTION = DB['FAQData']
+CHAT_DATA_COLLECTION = DB['ChatData']
+TOPIC_DATA_COLLECTION = DB['Topic']
+TEST_DATA = DB['TestData']
+TEST_OUTER_DATA = DB['TestOuterData']
+# specify time zone
+pacific = pytz.timezone('Asia/Taipei')
+
+
+
