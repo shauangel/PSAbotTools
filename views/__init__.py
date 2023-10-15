914644df42c94aa686885c880b34385ee55bd7dc
angel <shauangel2000@gmail.com>
2023-10-16 05:02:44 +0800

    api update

--- views/__init__.py
+++ views/__init__.py
@@ -1,11 +1,13 @@
 from .text_analysis_api import text_analysis_api
 from .stack_data_api import stack_data_api
 from .data_manager_api import data_manager_api
+from .outer_search_api import outer_search_api
 
 blueprint_prefix = [
     (text_analysis_api, "/api"),
-    (stack_data_api, "/api"),
-    (data_manager_api, "/api")
+    (data_manager_api, "/api"),
+    (outer_search_api, "/api"),
+    (stack_data_api, "/SO_api")
 ]
 
 
--- views/stack_data_api.py
+++ views/stack_data_api.py
@@ -1,4 +1,31 @@
 from flask import request, Blueprint, jsonify
+from models import stack_overflow_parser
 
 # register api
 stack_data_api = Blueprint("stack_data_api", __name__)
+
+
+@stack_data_api.route("/get_items", methods=['POST'])
+def get_items():
+    data = request.get_json()
+    # ip = request.remote_user
+    # print("Test ip detecting: " )
+    # print(ip)
+    try:
+        stack_data = stack_overflow_parser.StackData(data["urls"])
+        response = {"items": stack_data.get_results()}
+    except Exception as e:
+        response = {"error": e.__class__.__name__ + " : " + e.args[0]}
+    return jsonify(response)
+
+
+@stack_data_api.route("/download", methods=["POST"])
+# download detail of SO posts to local file system
+def download():
+    data = request.get_json()
+    # ... still working ...
+    return jsonify([])
+
+
+
+# future work: flask-cache -> temporary saving user's request & fetching results
