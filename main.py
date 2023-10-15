73d7b492ada98e4c78c91942c84a3b6b171ee998
angel <shauangel2000@gmail.com>
2023-10-16 00:24:59 +0800

    basic flask framework

--- main.py
+++
@@ -1,7 +0,0 @@
-import models.text_analysisV0
-import models._db
-
-
-if __name__ == '__main__':
-    # testing
-    print("Hey :D")
---
+++ toolbox.py
@@ -0,0 +1,3 @@
+# basic flask framework
+from flask import Flask
+from views import register_blueprint
