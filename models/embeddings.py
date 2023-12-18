6b05b236347a28f43060cfb0e7247bc9620577e1
Angel Shau <shauangel2000@gmail.com>
2023-12-19 07:53:46 +0800

    update

--- models/embeddings.py
+++ models/embeddings.py
@@ -3,6 +3,7 @@ from models import config
 import numpy
 
 
+# Request embeddings from embedding server
 def embeds(tokens):
     resp = requests.post(config.EMBEDDINGS_URL,
                          json={"doc_list": tokens})
